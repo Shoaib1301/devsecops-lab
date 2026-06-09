0edf49f99cc66bab2027f9f73ccd05029b97740b
Muhammad Shoaib <shoaibraju777@gmail.com>
2026-06-09 15:21:20 +0500

    Initial Commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express=require("express"); 
+ 
+const API_KEY="sk-test-123456789"; 
+ 
+const app=express(); 
+ 
+app.get("/",(req,res)=>{ 
+ 
+res.send("DevSecOps Security Lab"); 
+ 
+}); 
+ 
+app.listen(3000);
